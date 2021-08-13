f89c55096cebb8de3675d32b57742bade8419635
phuongnguyen1102 <54350974+phuongnguyen1102@users.noreply.github.com>
2021-08-13 16:16:09 +0700

    update localize + fix bug

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -12,7 +12,7 @@ export class ListsComponent implements OnInit {
   members: Partial<Member[]>;
   predicate = 'liked';
   pageNumber = 1;
-  pageSize = 5;
+  pageSize = 10;
   pagination: Pagination;
 
   constructor(private memberService: MembersService) { }
